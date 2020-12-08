4c5f693e816372aa037856d79316d197b9fd0569
Balakumaran-Bala <balakumaran55@gmail.com>
2020-12-08 12:29:27 -0800

    implement replay buffer

--- training_scripts/train_dqn.py
+++ training_scripts/train_dqn.py
@@ -9,10 +9,20 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
 
+import random
 import os
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 SEED = 2020
+ɣ = 1
+MAX_EPISODES = 1000
+N_STEPS = 1000
+
+LR_BEGIN = 0.01
+LR_END = 0.01
+
+EPS_BEGIN = 0.1
+EPS_END = 0.05
 
 class DQN():
     def __init__(self, state_dim, action_dim, hidden_dim=64, learning_rate=0.05):
@@ -38,27 +48,51 @@ class DQN():
         loss.backward()
         self.optimizer.step()
 
-        # loss = self.criterion(prediction, Variable(target, requires_grad=True))
-        # self.optimizer.zero_grad()
-        # loss.backward()
-        # self.optimizer.step()
-
     def print_weights(self):
         for param in self.model.parameters():
             print(param.data)
 
-ɣ = 1
-MAX_EPISODES = 200
-N_STEPS = 1000
 
-LR_BEGIN = 0.01
-LR_END = 0.01
+class ReplayBuffer():
+    def __init__(self, len):
+        self.replay_buffer = deque(maxlen=len)
 
-EPS_BEGIN = 0.1
-EPS_END = 0.05
+    def push(self, state, reward, done, info):
+        self.replay_buffer.append((state, reward, done, info))
 
-env = gym.make("CartPole-v1")
-env.seed(SEED)
+    def update(self, observation, state_next, model):
+        experience_state, reward, done, info = observation
+
+        q_values = model.predict(experience_state)
+        q_values_next = model.predict(next_state)
+
+        target = q_values.tolist()
+        target[action] = reward + ɣ*torch.max(q_values_next).item()
+
+        if done:
+            target[action] = reward
+            model.update(experience_state, target)
+            return
+
+        model.update(experience_state, target)
+
+    def replay(self, size, state_next, model):
+        length = len(self.replay_buffer)
+
+        if length < size:
+            batch = random.sample(list(self.replay_buffer), int(len(self.replay_buffer)/2))
+        else:
+            batch = random.sample(list(self.replay_buffer), size)
+
+        if len(batch) == 0:
+            self.update(self.replay_buffer[-1], state_next, model)
+        else:
+            for obs in batch:
+                self.update(obs, state_next, model)
+
+    def clear(self):
+        self.replay_buffer.clear()
+
 
 def get_action_from_policy(model, state, t):
     if t <= N_STEPS:
@@ -74,14 +108,17 @@ def get_action_from_policy(model, state, t):
     return action
 
 
+env = gym.make("CartPole-v1")
+env.seed(SEED)
+
+
 if __name__ == "__main__":
-    replay_buffer = deque(maxlen=50)
+    replay_buffer = ReplayBuffer(50)
 
     t = 0
     plot_episodes = []
     plot_rewards = []
 
-    print(env.observation_space.shape[0])
     model = DQN(env.observation_space.shape[0], env.action_space.n, 50, 0.001)
 
     for episode in range(MAX_EPISODES):
@@ -90,27 +127,31 @@ if __name__ == "__main__":
         total_reward = 0
         done = False
 
+        # replay_buffer.clear()
         while not done:            
             action = get_action_from_policy(model, current_state, t)
 
             next_state, reward, done, info = env.step(action)
 
-            replay_buffer.append(current_state)
-            replay_state = replay_buffer[np.random.randint(0, high=len(replay_buffer))]
+            replay_buffer.push(current_state, reward, done, info)
+            replay_buffer.replay(20, next_state, model)
 
 
-            q_values = model.predict(current_state)
-            q_values_next = model.predict(next_state)
+            # replay_buffer.append(current_state)
+            # replay_state = replay_buffer[np.random.randint(0, high=len(replay_buffer))]
 
-            target = q_values.tolist()
-            target[action] = reward + ɣ*torch.max(q_values_next).item()
+            # q_values = model.predict(current_state)
+            # q_values_next = model.predict(next_state)
 
-            if done:
-                target[action] = reward
-                model.update(current_state, target)
-                break
+            # target = q_values.tolist()
+            # target[action] = reward + ɣ*torch.max(q_values_next).item()
 
-            model.update(current_state, target)
+            # if done:
+            #     target[action] = reward
+            #     model.update(current_state, target)
+            #     break
+
+            # model.update(current_state, target)
 
             current_state = next_state
             total_reward += reward
--- training_scripts/train_lfa.py
+++ training_scripts/train_lfa.py
@@ -3,13 +3,13 @@ import slimevolleygym
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import deque
-
+import random
 
 ɣ = 1
 target_update_freq = 1
 N_STEPS = 1000
 
-MAX_EPISODES = 500
+MAX_EPISODES = 1000
 
 SEED = 2020
 LR_BEGIN = 0.01
@@ -18,15 +18,8 @@ LR_END = 0.01
 EPS_BEGIN = 0.1
 EPS_END = 0.05
 
-
-# env = gym.make("SlimeVolley-v0")
 env = gym.make("CartPole-v1")
 env.seed(SEED)
-
-# action_space = {0: [1,0,0], 1: [0,1,0], 2: [0,0,1],
-#                 3: [1,0,1], 4: [0,1,1], 5: [0,0,0]}
-
-# action_space = {0: [0, 1], 1: [1, 0]}
 
 def get_action_from_policy(policy_weights, state, ε):
     if t <= N_STEPS:
@@ -41,11 +34,59 @@ def get_action_from_policy(policy_weights, state, ε):
     out = i
     return out
 
+class ReplayBuffer():
+    def __init__(self, len):
+        self.replay_buffer = deque(maxlen=len)
+
+    def push(self, state, reward, done, info):
+        self.replay_buffer.append((state, reward, done, info))
+
+    def get_gradient(self, observation, state_next, w):
+        state_exp, reward, done, info = observation
+        q_values = np.matmul(w.T, state_exp)
+        q_values_next = np.matmul(w.T, state_next)
+
+        state_exp = np.reshape(state_exp, (4,1))
+
+        target = q_values.copy()
+        if not done:
+            target[action] = reward + ɣ*np.max(q_values_next)
+        else:
+            target[action] = reward
+
+        loss = target - q_values
+        loss = np.reshape(loss, (1, 2))
+        gradient = 𝛼 * np.matmul(state_exp, loss)
+
+        norm = np.linalg.norm(gradient)
+        if norm > 10:
+            gradient *= 10 / norm;
+
+        return gradient
+
+    def replay(self, size, state_next, w):
+        length = len(self.replay_buffer)
+
+        if length < size:
+            batch = random.sample(list(self.replay_buffer), int(len(self.replay_buffer)/2))
+        else:
+            batch = random.sample(list(self.replay_buffer), size)
+
+        if len(batch) == 0:
+            gradient = self.get_gradient(self.replay_buffer[-1], state_next, w)
+            w += gradient
+        else:
+            for obs in batch:
+                gradient = self.get_gradient(obs, state_next, w)
+                w += gradient
+
+        return w
+
+    def clear(self):
+        self.replay_buffer.clear()
+
 
 if __name__ == "__main__":
-    # w_π = np.random.rand(4, 2) # original policy we are using
-    # w = np.random.rand(4,2) # shit there are way more than 3 actions...
-
     w_π = np.zeros((4,2))
     w = np.zeros((4,2))
     w_target = np.random.rand(4,2)
@@ -53,7 +94,7 @@ if __name__ == "__main__":
     𝛼 = LR_BEGIN
     ε = EPS_BEGIN
 
-    replay_buffer = deque(maxlen=50)
+    replay_buffer = ReplayBuffer(50)
 
     episodes = 0
     plot_episodes = []
@@ -62,41 +103,17 @@ if __name__ == "__main__":
         state_current = env.reset()
         total_reward = 0
         done = False
+
+        # replay_buffer.clear()
         while not done:
             t += 1
 
             action = get_action_from_policy(w_π, state_current, ε)
             state_next, reward, done, info = env.step(action)
 
-            replay_buffer.append(state_current)
-            state_exp = replay_buffer[np.random.randint(0, high=len(replay_buffer))]
+            replay_buffer.push(state_current, reward, done, info)
+            w = replay_buffer.replay(20, state_next, w)
 
-            state_exp = np.reshape(state_exp, (4, 1))
-
-            q_values = np.matmul(w.T, state_exp)
-            q_values_next = np.matmul(w.T, state_next)
-
-            target = q_values.copy()
-
-            if not done:
-                target[action] = reward + ɣ*np.max(q_values_next)
-            else:
-                target[action] = reward
-
-            loss = target - q_values
-
-            loss = np.reshape(loss, (1, 2))
-            state_current = np.reshape(state_current, (4,1))
-
-            gradient = 𝛼 * np.matmul(state_exp, loss)
-
-            norm = np.linalg.norm(gradient)
-
-            if norm > 10:
-                gradient *= 10 / norm;
-
-            # print(w)
-            w += gradient
             w_π = w.copy()
 
             state_current = state_next
